380e0f74c4efa5787c38e4a71abe844610c7a823
mrutunjay mahanta <mrutunjay mahanta@LAPTOP-PU89I2QO>
2023-09-08 12:17:45 +0530

    Add project files.

---
+++ ExampleUI/Form2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleUI
+{
+    internal class Form2
+    {
+    }
+}
